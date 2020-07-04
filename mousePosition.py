6e4e31955537149eac9cd5b30ded0201408fdd71
Lokikaku <43895783+Lokikaku@users.noreply.github.com>
2020-07-04 02:48:19 -0500

    Update mousePosition.py
    this ver. needs the keyboard package but also gives the user the ability to quit the script using the "esc" or "q" keys without having to Red [X] or Alt-F4

--- mousePosition.py
+++ mousePosition.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import keyboard
 
 noYeet = True
 period = "."
@@ -8,7 +9,7 @@ try:
         x, y = pyautogui.position()
         xArchive = x
         yArchive = y
-        time.sleep(0.2)
+        time.sleep(0.1)
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(xArchive) + ' Y: ' + str(yArchive)
         if x != xArchive:
@@ -19,5 +20,14 @@ try:
             with open('mousePos1.txt', 'a') as f:
                 f.write(period)
                 f.write("\n")
+        if keyboard.is_pressed('esc'): 
+            break
+        else:
+            pass
+        if keyboard.is_pressed('p'): 
+            noYeet = False
+        else:
+            pass
 except KeyboardInterrupt:
     print('\n')
+# Hopefully this edit works :P and ads an escape route if things go south, which yes, they can.
